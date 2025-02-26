b53e842f4988c5d587a584c9f5907767a341cd21
Guillaume Zhu <guillaume.zhu@outlook.com>
2025-02-26 18:22:27 +0100

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <header>header</header>
+
+    <main>
+      <RouterView />
+    </main>
+
+    <footer>footer</footer>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <h1>Hello</h1>
+  <h2>Je suis un h2</h2>
+  <p>Je suis un p</p>
+</template>
+
+<style scoped></style>
