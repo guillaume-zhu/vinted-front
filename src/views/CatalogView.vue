89149b27adf9eea668e05117f53cdc5b09e3d63f
Guillaume Zhu <guillaume.zhu@outlook.com>
2025-04-17 16:39:57 +0200

    ♻️ refactor(catalog): improve offer fetch logic using category name filter for any level

--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
-import Breadcrumb from '@/components/Breadcrumb.vue'
 
 const props = defineProps({
   id: {
@@ -12,21 +11,22 @@ const props = defineProps({
 console.log('main category id ---->', props.id)
 
 // Variables
-const categoriesId = ref([])
+const categoryData = ref([])
 const offersList = ref([])
 const isLoading = ref(false)
 const allCategoriesId = []
 
-const collectCategoryIds = (category) => {
-  if (!allCategoriesId.includes(category.id)) {
-    allCategoriesId.push(category.id)
-  }
+// Fonction récursive pour trouver les id en arborescence
+// const collectCategoryIds = (category) => {
+//   if (!allCategoriesId.includes(category.id)) {
+//     allCategoriesId.push(category.id)
+//   }
 
-  if (category?.attributes?.children?.data?.length > 0)
-    for (const child of category.attributes.children.data) {
-      collectCategoryIds(child)
-    }
-}
+//   if (category?.attributes?.children?.data?.length > 0)
+//     for (const child of category.attributes.children.data) {
+//       collectCategoryIds(child)
+//     }
+// }
 
 // Requete pour récupérer les id de la category et des enfants
 onMounted(async () => {
@@ -35,19 +35,14 @@ onMounted(async () => {
     const response = await axios.get(
       `http://localhost:1337/api/categories/${props.id}?populate[0]=children&populate[1]=children.children&populate[2]=children.children.children&populate[3]=children.children.children.children`,
     )
-    categoriesId.value = response.data.data
-    console.log('categoriesId ---->', categoriesId.value)
+    categoryData.value = response.data.data
+    console.log('categoryData ---->', categoryData.value)
 
-    collectCategoryIds(categoriesId.value)
-    console.log('allCategoriesId ---->', allCategoriesId)
-
-    const categoriesQuery = allCategoriesId
-      .map((id, index) => `&filters[$or][${index}][category][id][$eq]=${id}`)
-      .join('')
-    // console.log('categoriesQuery ---->', categoriesQuery)
+    const categoryName = categoryData.value.attributes.name
+    console.log('categoryName ---->', categoryName)
 
     const responseOffers = await axios.get(
-      `http://localhost:1337/api/offers?populate=*${categoriesQuery}&pagination[pageSize]=96`,
+      `http://localhost:1337/api/offers?populate=*&filters[category][name][$containsi]=${categoryName}&pagination[pageSize]=96`,
     )
 
     offersList.value = responseOffers.data
@@ -57,18 +52,17 @@ onMounted(async () => {
   }
 })
 
-// Récupérer props id de la catégorie selectionnée
-// Faire une requête pour récupérer un tableau d'id des catégories ainsi que de tout les enfants puis stocker dans une variable
-// Fonction pour récupérer les id des enfants et de leurs enfants
-// Faire une requête pour chercher les offres avec tout les id de categoriesId + pagination 96 offres par page
-// -
-
-// Stocker la réponse dans une variable
-// Faire une requête pour récupérer dressing en ciblant une des offres parmis offersList puis remonter a avatar pour obtenir un dressing
+// récupérer le nom de la catégorie
+// faire une requête des offres qui contient le nom de la catégorie par 96
+// afficher les offres avec une pagination
 </script>
 
 <template>
-  <div class="container">Catalog</div>
+  <div class="container">
+    <p v-if="categoryData.attributes">
+      {{ categoryData.attributes.name }}
+    </p>
+  </div>
 </template>
 
 <style scoped></style>
