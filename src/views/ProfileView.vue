eed667c192be9cb0d7d105ef3f0c4fe60439b7a9
Guillaume Zhu <guillaume.zhu@outlook.com>
2025-07-18 19:45:23 +0200

    ✨ feat(profile): add mobile display section for offers and user info

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -11,6 +11,7 @@ console.log('userInfoCookie ---->', GlobalStore.userInfoCookie.value)
 const userInfo = ref('')
 const isLoading = ref(false)
 const errorMessage = ref('')
+const actualSection = ref(null)
 const props = defineProps({
   id: {
     type: String,
@@ -38,113 +39,154 @@ onMounted(async () => {
 
 <template>
   <div class="container profile" v-if="userInfo">
-    <div class="profile__details">
-      <img :src="userInfo.avatar.url" alt="" v-if="userInfo.avatar?.url" />
-      <img src="../assets/img/user-profile.webp" alt="" v-else />
+    <!-- SECTIONS ------>
+    <div class="profile__sections">
+      <div class="sections" @click="actualSection = 1">
+        <span>Annonces</span>
+      </div>
+      <div class="sections" @click="actualSection = 2">
+        <span>À propos</span>
+      </div>
+    </div>
 
-      <div class="profile__infos">
-        <div class="profile__infos-name-edit">
-          <h1>{{ userInfo.username }}</h1>
+    <!-- OFFERS SECTION ------>
+    <!-- PROFILE DETAILS -->
+    <div class="profile__offers" v-if="actualSection === 1">
+      <div class="profile__details">
+        <img :src="userInfo.avatar.url" alt="" v-if="userInfo.avatar?.url" />
+        <img src="../assets/img/user-profile.webp" alt="" v-else />
 
-          <RouterLink
-            :to="{ name: 'settings' }"
-            v-if="
-              GlobalStore.userInfoCookie.value &&
-              GlobalStore.userInfoCookie?.value?.id.toString() === id
-            "
-          >
-            <button><font-awesome-icon :icon="['fas', 'pen']" /> Modifier mon profil</button>
-          </RouterLink>
-        </div>
+        <div class="profile__infos">
+          <div class="profile__infos-name-edit">
+            <h1>{{ userInfo.username }}</h1>
 
-        <div class="profile__infos-about">
-          <div>
-            <p>À propos</p>
-
-            <font-awesome-icon :icon="['fas', 'map-marker-alt']" />
-            <span
-              >{{ userInfo.city[0].toUpperCase() + userInfo.city.slice(1) }},
-              {{ userInfo.country.displayName }}</span
+            <RouterLink
+              :to="{ name: 'settings' }"
+              v-if="
+                GlobalStore.userInfoCookie.value &&
+                GlobalStore.userInfoCookie?.value?.id.toString() === id
+              "
             >
+              <button><font-awesome-icon :icon="['fas', 'pen']" /> Modifier mon profil</button>
+            </RouterLink>
           </div>
 
-          <div>
-            <p>Informations verifiées</p>
-            <font-awesome-icon :icon="['fas', 'check-circle']" />
-            <span>Email</span>
+          <div class="profile__infos-about">
+            <div>
+              <p>À propos</p>
+
+              <font-awesome-icon :icon="['fas', 'map-marker-alt']" />
+              <span
+                >{{ userInfo.city[0].toUpperCase() + userInfo.city.slice(1) }},
+                {{ userInfo.country.displayName }}</span
+              >
+            </div>
+
+            <div>
+              <p>Informations verifiées</p>
+              <font-awesome-icon :icon="['fas', 'check-circle']" />
+              <span>Email</span>
+            </div>
+          </div>
+
+          <div class="profile__infos-description">
+            <p v-if="userInfo.description">{{ userInfo.description }}</p>
           </div>
         </div>
+      </div>
 
-        <div class="profile__infos-description">
-          <p v-if="userInfo.description">{{ userInfo.description }}</p>
+      <!-- DRESSING -->
+      <div class="dressing" v-if="userInfo.offers">
+        <div>
+          <h3>Dressing</h3>
+        </div>
+
+        <h2>
+          {{ userInfo.offers.length > 0 ? userInfo.offers.length + ' articles' : `Pas d'articles` }}
+        </h2>
+
+        <div class="dressing__content">
+          <OfferCard
+            v-for="offer in userInfo.offers"
+            :key="offer.id"
+            :offer="offer"
+            :fromProfile="true"
+            :disablePricePopup="true"
+          />
         </div>
       </div>
     </div>
 
-    <div class="dressing" v-if="userInfo.offers">
-      <div>
-        <h3>Dressing</h3>
-      </div>
+    <!-- ABOUT SECTION ----->
+    <div class="profile__about" v-if="actualSection === 2">
+      <div class="profile__about-details">
+        <img :src="userInfo.avatar.url" alt="" v-if="userInfo.avatar?.url" />
+        <img src="../assets/img/user-profile.webp" alt="" v-else />
 
-      <h2>
-        {{ userInfo.offers.length > 0 ? userInfo.offers.length + ' articles' : `Pas d'articles` }}
-      </h2>
+        <div class="profile__about-infos">
+          <div class="profile__about-infos-name-edit">
+            <h1>{{ userInfo.username }}</h1>
 
-      <div class="dressing__content">
-        <OfferCard
-          v-for="offer in userInfo.offers"
-          :key="offer.id"
-          :offer="offer"
-          :fromProfile="true"
-          :disablePricePopup="true"
-        />
+            <RouterLink
+              :to="{ name: 'settings' }"
+              v-if="
+                GlobalStore.userInfoCookie.value &&
+                GlobalStore.userInfoCookie?.value?.id.toString() === id
+              "
+            >
+              <button><font-awesome-icon :icon="['fas', 'pen']" /> Modifier mon profil</button>
+            </RouterLink>
+          </div>
+
+          <div class="profile__about-infos-about">
+            <div>
+              <p>À propos</p>
+
+              <font-awesome-icon :icon="['fas', 'map-marker-alt']" />
+              <span
+                >{{ userInfo.city[0].toUpperCase() + userInfo.city.slice(1) }},
+                {{ userInfo.country.displayName }}</span
+              >
+            </div>
+
+            <div>
+              <p>Informations verifiées</p>
+              <font-awesome-icon :icon="['fas', 'check-circle']" />
+              <span>Email</span>
+            </div>
+          </div>
+
+          <div class="profile__about-infos-description">
+            <p v-if="userInfo.description">{{ userInfo.description }}</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 
-  <p v-else>En cours de chargement</p>
+  <h1 v-else>En cours de chargement</h1>
 </template>
 
 <style scoped>
-/* PROFILE ------------------------------------ */
-.profile__details {
-  border: 1px solid blue;
-  display: flex;
-  width: 100%;
-  height: 100%;
+/* SMALL / MOBILE (< 720px) */
+.profile {
+  padding: 0px 10px;
 }
 
-/* DETAIL -------------------- */
-.profile__details > img {
-  width: 192px;
-  height: 192px;
-  border-radius: 500px;
-  object-fit: cover;
+/* PROFILE DETAILS -------------------------- */
+img {
+  width: 40px;
+  height: 40px;
+}
+/* MEDIUM (>= 720px ) */
+@media (min-width: 720px) {
 }
 
-.profile__infos {
-  border: 1px solid red;
-  /* width: 100%; */
-  flex: 1;
-  height: 100%;
+/* DESKTOP (>= 960px) */
+@media (min-width: 960px) {
 }
 
-.profile__infos-name-edit {
-  border: 1px solid peru;
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-}
-
-.profile__infos-about {
-  display: flex;
-  gap: 50px;
-}
-
-/* DRESSING ------------------ */
-.dressing__content {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 15px;
+/* DESKTOP LARGE (>= 1200px) */
+@media (min-width: 1200px) {
 }
 </style>
